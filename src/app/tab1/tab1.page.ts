68a0627d54ecf2fad3c1aaa4fcfe702fc1e09378
JoaoPedroBispo <jopbispo83@gmail.com>
2023-03-09 14:39:43 -0300

    Animação
    Criação da animação do cara ou coroa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,19 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedStated', [
+      state('default', style({transform: 'rotateY(0)'})),
+      state('rotated', style({transform: 'rotateY(-360deg)'})),
+      transition('rotated => default', animate('2000ms ease-out')),
+      transition('default => rotated', animate('2000ms ease-in')),
+
+    ]),
+  ],
 })
 export class Tab1Page {
 
@@ -12,10 +22,17 @@ export class Tab1Page {
   logo = 'assets/logo.png';
   image = this.logo;
   info = 'Clique no botão para jogar!';
+  state = 'default';
 
   constructor() {}
 
   jogarMoeda(){
+    this.image = this.logo;
+    this.info = 'Girando...';
+    this.state = this.state === 'default' ? 'rotated' : 'default';
+
+    setTimeout(()=>{
+
     if (Math.random() < 0.5){
       this.image = this.cara;
       this.info = 'Cara!';
@@ -23,6 +40,7 @@ export class Tab1Page {
       this.image = this.coroa;
       this.info = 'Coroa!'
     }
-  }
+    }, 2000);
 
+  }
 }
